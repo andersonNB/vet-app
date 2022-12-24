476d40fe26846b6ede52a7d5881d3885298bb750
Diego <diego@Diegos-Laptop.local>
2022-12-24 12:27:15 -0500

    update: scss added

--- src/VetApp.jsx
+++ src/VetApp.jsx
@@ -1,6 +1,6 @@
 import {BrowserRouter} from 'react-router-dom';
 // import reactLogo from './assets/react.svg'
-// import './App.css'
+import './VetApp.scss'
 import { VetRoutes } from "./routes";
 
 function VetApp() {
