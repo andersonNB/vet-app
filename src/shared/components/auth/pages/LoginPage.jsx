65eb5a58aaccb11aca0eabaf240130b0cd03f30a
andersonNB <andersonaugustonb@ufps.edu.co>
2022-12-25 19:16:15 -0500

    Maquetación inicial Login

--- src/shared/components/auth/pages/LoginPage.jsx
+++ src/shared/components/auth/pages/LoginPage.jsx
@@ -1,45 +1,52 @@
+import { Link } from "react-router-dom";
+import './LoginPage.scss'
+import loginDog from '../../../../assets/loginDog.png';
+
 export const LoginPage = () => {
 	return (
 		<>
-      <div className="container">
-      <div style={{backgroundColor:"#F1F7B5"}} className="row">
-      <div className="col align-self-center">
-      <h3>Iniciar Sesión</h3>
-			<form>
-				<div class="mb-3">
-					<label for="exampleInputEmail1" class="form-label">
-						Correo:
-					</label>
-					<input
-						type="email"
-						class="form-control"
-						id="exampleInputEmail1"
-						aria-describedby="emailHelp"
-					/>
-				</div>
-				<div class="mb-3">
-					<label for="exampleInputPassword1" class="form-label">
-						Contraseña:
-					</label>
-					<input
-						type="password"
-						class="form-control"
-						id="exampleInputPassword1"
-					/>
-				</div>
-				<div class="mb-3 form-check">
-					<input type="checkbox" class="form-check-input" id="exampleCheck1" />
-					<label class="form-check-label" for="exampleCheck1">
-						Check me out
-					</label>
-				</div>
-				<button type="submit" class="btn btn-primary">
-					Iniciar Sesión
-				</button>
-        <a>¿Olvide mi contraseña</a>
-			</form>
-      </div>
-    </div>
+      <div className="container fondoImage">
+        <div  className="row align-items-center justify-content-center vh-100">
+          <div className="col-6">
+              <form className="formLogin">
+              <h3>Iniciar Sesión</h3>
+                <div className="mb-3">
+                  <label className="form-label">
+                    Correo:
+                  </label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label">
+                    Contraseña:
+                  </label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="exampleInputPassword1"
+                  />
+                </div>
+                {/* <div className="mb-3 form-check">
+                  <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                  <label className="form-check-label" for="exampleCheck1">
+                    Check me out
+                  </label>
+                </div> */}
+                <button type="submit" className="btn btn-primary">
+                  Iniciar Sesión
+                </button>
+                <Link to="#">¿Olvide mi contraseña?</Link>
+              </form>
+          </div>
+          <div className="col-6">
+            <img src={loginDog} className="loginDogImage" loading="lazy"/>
+          </div>
+        </div>
       </div>
 		</>
 	);
