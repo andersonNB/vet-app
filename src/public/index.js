ed4d64076e8c185eed7d945dd19902bdc35a1492
Diego <diego@Diegos-Laptop.local>
2023-01-23 21:36:06 -0500

    changes: footer and stepBystep added with others modifications

--- src/public/index.js
+++ src/public/index.js
@@ -1,2 +1,4 @@
 export { default as BodyComponent } from './components/body/BodyComponent';
+export { default as Footer } from './components/footer/Footer';
+export { default as StepByStep } from './components/stepByStep/StepByStep';
 export { default as HomePage } from './pages/home/HomePage';
