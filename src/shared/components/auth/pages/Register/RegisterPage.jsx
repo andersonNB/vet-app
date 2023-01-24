f155f4d97b47751669826bbfb9ed0fbda9fe7353
andersonNB <andersonaugustonb@ufps.edu.co>
2023-01-23 21:46:42 -0500

    UPDATE: Css styles in RegisterPage

--- src/shared/components/auth/pages/Register/RegisterPage.jsx
+++ src/shared/components/auth/pages/Register/RegisterPage.jsx
@@ -1,80 +1,93 @@
-import { Link } from "react-router-dom"
+import {Link} from "react-router-dom";
 import ImageRegister from "../../../../../assets/verify.png";
 import "./RegisterPage.scss";
 
 export const RegisterPage = () => {
-  return (
-    <>
-      <div className="backgroundImageRegister text-center" style={{minHeight:"100vh", maxWidth:"100%"}}>
-        <div  className="row align-items-center justify-content-center">
-          <div className="col-sm-12 col-md-6 col-12">
-            <form className="formLogin">
-                <p style={{backgroundColor:"cornflowerblue", display:"inline-block"}} >Empieza Gratis</p>
-              <h3>Crear nueva cuenta</h3>
-              <div className="row">
-                    <div className="col-6" style={{display:"flex",alignItems:"center", }}>
-                        Nombre: 
-                        <input
-                        type="text"
-                        className="form-control inputText"
-                        id="name"
-                        placeholder="nombre"
-                    />
-                    </div>
-                    <div className="col-6" style={{display:"flex",alignItems:"center"}}>
-                        Apellido:
-                        <input
-                        type="text"
-                        className="form-control inputText"
-                        id="apellido"
-                        placeholder="Apellido"
-                    />
-                    </div>
-              </div>
-                <div className="mb-3">
-                  <label className="form-label">
-                    Correo:
-                  </label>
-                  <input
-                    type="email"
-                    className="form-control inputText"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                    placeholder="Ej@example.com"
-                  />
-                </div>
-                <div className="mb-3">
-                  <label className="form-label">
-                    Contraseña:
-                  </label>
-                  <input
-                    type="password"
-                    className="form-control inputText"
-                    id="exampleInputPassword2"
-                    placeholder="*******"
-                  />
-                </div>
-                <div className="mb-3">
-                  <label className="form-label">
-                    Confirmar Contraseña:
-                  </label>
-                  <input
-                    type="password"
-                    className="form-control inputText"
-                    id="exampleInputPassword1"
-                    placeholder="*******"
-                  />
-                </div>
-                
-                  <button type="submit" className="btn btn-primary box">
-                   Crear Cuenta
-                    <img src={ImageRegister} style={{marginLeft:"6px",width: '25px', height: '25px'}} />
-                  </button>
-                  <Link to="/" className="goHome m-3">Página Inicial</Link>
-              </form>
-          </div>
-        </div>
-      </div>
+	return (
+		<>
+			<div
+				className="backgroundImageRegister text-center"
+				style={{minHeight: "100vh", maxWidth: "100%"}}
+			>
+				<div className="row align-items-center justify-content-center">
+					<div className="col-sm-12 col-md-6 col-12">
+						<form className="formLogin">
+							<p>Empieza Gratis</p>
+							<h3 style={{fontWeight:"900"}}>Crear nueva cuenta</h3>
+							<div className="row">
+								<div className="col-6">
+									<div className="InputForm">
+										<input
+											type="text"
+											className="form-control inputText"
+											id="name"
+											placeholder="Nombre"
+											style={{width: "100%"}}
+										/>
+									</div>
+								</div>
+								<div className="col-6">
+									<div className="InputForm">
+										<input
+											type="text"
+											className="form-control inputText"
+											id="apellido"
+											placeholder="Apellido"
+											style={{width: "100%"}}
+										/>
+									</div>
+								</div>
+							</div>
+							<div className="InputForm">
+								<input
+									type="email"
+									className="form-control inputText"
+									id="exampleInputEmail1"
+									aria-describedby="emailHelp"
+									placeholder="Correo Electronico"
+									style={{width: "100%"}}
+								/>
+							</div>
+							<div className="InputForm">
+								<input
+									type="password"
+									className="form-control inputText"
+									id="exampleInputPassword2"
+									placeholder="Contraseña"
+									style={{width: "100%"}}
+								/>
+							</div>
+							<div className="InputForm">
+								<input
+									type="password"
+									className="form-control inputText"
+									id="exampleInputPassword1"
+									placeholder="Confirmar Contraseña"
+									style={{width: "100%"}}
+								/>
+							</div>
+							<div className="row">
+								<div className="col" style={{backgroundColor:"cornflowerblue"}}>
+									<button type="submit" className="btn btn-primary box">
+										Crear Cuenta
+										<img
+											src={ImageRegister}
+											style={{marginLeft: "6px", width: "25px", height: "25px"}}
+										/>
+									</button>
+								</div>
+								<div className="col" style={{backgroundColor:"grey"}}>
+									<button className="btn btn-primary box">
+										<Link to="/" className="goHome">
+											Página Inicial
+										</Link>
+									</button>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
 		</>
-  )
-}
+	);
+};
