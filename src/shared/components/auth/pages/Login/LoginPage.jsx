a53e4f28e80fc3da6f344df560ec30ceaca86040
Anderson Navarro <andersonaugustonb@ufps.edu.co>
2023-02-18 21:08:59 -0500

    FIX: creation a ForgotPassword.scss file for the css in componente FogotPassword.jsx, fix the button with the overflow text

--- src/shared/components/auth/pages/Login/LoginPage.jsx
+++ src/shared/components/auth/pages/Login/LoginPage.jsx
@@ -68,49 +68,6 @@ export const LoginPage = () => {
 							</div>
 						</form>
 					</div>
-					{/* <div className="col-sm-12 col-md-6 col-12">
-						<form className="">
-							<div class="mb-3 formLogin">
-								<label for="exampleInputEmail1" class="form-label">
-									Email address
-								</label>
-								<input
-									type="email"
-									class="form-control"
-									id="exampleInputEmail1"
-									aria-describedby="emailHelp"
-								/>
-								<div id="emailHelp" class="form-text">
-									We'll never share your email with anyone else.
-								</div>
-
-								<label for="exampleInputPassword1" class="form-label">
-									Password
-								</label>
-								<input
-									type="password"
-									class="form-control"
-									id="exampleInputPassword1"
-								/>
-
-								<div class="mb-3 form-check">
-									<input
-										type="checkbox"
-										class="form-check-input"
-										id="exampleCheck1"
-									/>
-									<label class="form-check-label" for="exampleCheck1">
-										Check me out
-									</label>
-								</div>
-								<button type="submit" class="btn btn-primary">
-									Submit
-								</button>
-							</div>
-							<ForgotPassword />
-						</form>
-					</div> */}
-
 					<div className="col-md-6 col-6" style={{height: "100vh"}}>
 						<SvgComponentLogin />
 					</div>
